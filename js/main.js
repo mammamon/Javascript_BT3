0cefdaf7d68a56318482d385ba35c94f97a08514
mammamon <quangnguyenonlinevn@gmail.com>
2023-05-25 16:30:55 +0700

    BT6 COMPLETED

--- js/main.js
+++ js/main.js
@@ -2,21 +2,16 @@
 var result = document.getElementById('result');
 var array = [];
 var btnAll = document.querySelectorAll('button:not(.no-overlay)');
-//SHOW BUTTON OVERLAY WHILE HOVERING + ALERT WHEN ARRAY EMPTY
+//SHOW BUTTON OVERLAY WHILE HOVERING
 btnAll.forEach((button, index) => {
     button.setAttribute('data-number', 'BT' + (index + 1));
-    button.addEventListener('click', () => {
-        if (array.length === 0) {
-            alert('CHƯA NHẬP SỐ')
-        }
-    })
 }
 );
 //ADD NUMBER BUTTON
 document.getElementById('arrayBtn').onclick = function () {
     var inputNum = parseFloat(document.getElementById("inputNum").value);
     if (isNaN(inputNum)) {
-        alert('SỐ NHẬP KHÔNG HỢP LỆ')
+        alert('SỐ NHẬP KHÔNG HỢP LỆ');
         return;
     } else {
         array.push(inputNum);
@@ -29,6 +24,14 @@ document.getElementById('arrayBtn').onclick = function () {
 document.getElementById('resetBtn').onclick = function () {
     array = [];
     document.getElementById("arrayTxt").placeholder = array;
+}
+// CHECK EMPTY ARRAY
+function emptyArray() {
+    if (array.length === 0) {
+        result.innerHTML = 'CHƯA NHẬP SỐ';
+        return true;
+    }
+    return false;
 }
 //BT1
 document.getElementById('sumEvenBtn').onclick = function () {
@@ -48,6 +51,7 @@ document.getElementById('countEvenBtn').onclick = function () {
 }
 //BT3
 document.getElementById('minBtn').onclick = function () {
+    if (emptyArray()) return;
     var min = array[0];
     for (var i = 0; i < array.length; i++) {
         if (min > array[i]) min = array[i];
@@ -56,6 +60,7 @@ document.getElementById('minBtn').onclick = function () {
 }
 //BT4
 document.getElementById('minPositiveBtn').onclick = function () {
+    if (emptyArray()) return;
     var minPositive = null;
     for (var i = 0; i < array.length; i++) {
         if (array[i] > 0 && (minPositive === null || array[i] < minPositive)) {
@@ -67,14 +72,41 @@ document.getElementById('minPositiveBtn').onclick = function () {
         }
     }
 }
-
 //BT5
 document.getElementById('lastEvenBtn').onclick = function () {
-    var lastEven = -1;
+    if (emptyArray()) return;
+    var lastEven = null;
     for (var i = 0; i < array.length; i++) {
         if (array[i] % 2 === 0) {
             lastEven = array[i];
             result.innerHTML = 'SỐ CHẴN CUỐI CÙNG LÀ ' + lastEven;
-        } else { result.innerHTML = lastEven }
+        }
+        if (lastEven === null) result.innerHTML = '(-1) MẢNG KHÔNG CÓ SỐ CHẴN';
     }
+}
+//BT6 TRIGGER MODAL
+document.getElementById('btn-modal').onclick = function () {
+    if (array.length < 2) {
+        var btnModal = document.getElementById('btn-modal');
+        btnModal.setAttribute('data-bs-target', " ");
+        result.innerHTML = 'VUI LÒNG NHẬP ÍT NHẤT 2 SỐ'
+        return;
+    }
+    btnModal = document.getElementById('btn-modal');
+    btnModal.setAttribute('data-bs-target', "#BT6");
+    btnModal.click();
+}
+//BT6
+document.getElementById('swapBtn').onclick = function () {
+    var index1 = +document.getElementById('index1').value;
+    var index2 = +document.getElementById('index2').value;
+    if (index1 > array.length || index2 > array.length) {
+        result.innerHTML = "VỊ TRÍ NHẬP KHÔNG HỢP LỆ"
+        return;
+    }
+    var originalIndex1 = array[index1 - 1];
+    array[index1 - 1] = array[index2 - 1];
+    array[index2 - 1] = originalIndex1;
+    document.getElementById("arrayTxt").placeholder = array.join(", ");
+    result.innerHTML = 'HOÁN ĐỔI VỊ TRÍ THỨ ' + index1 + ' VỚI VỊ TRÍ THỨ ' + index2;
 }
